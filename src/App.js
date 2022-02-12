35da828bb205c6f886a54e7f8723cde83a2e65a4
ManeeshChikoti <maneeshchikoti@gmail.com>
2022-02-13 01:35:25 +0530

    raect app

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import Plans from './Plans'
+import './App.css';
+
+function App() {
+  const plans=[
+  {
+    name:"FREE",
+    price:0,
+    features:[
+      {name:'Single User',enabled:true},
+      {name:'5GB Storage',enabled:true},
+      {name:'Unlimited Public Projects',enabled:true},
+      {name:'Community Access',enabled:true},
+      {name:'Unlimited Private Projects',enabled:false},
+      {name:'Dedicated Phone Support',enabled:false},
+      {name:'Free Subdomain',enabled:false},
+      {name:'Monthly Status Reports',enabled:false}
+    ]
+  },
+  {
+    name:"PLUS",
+    price: 9,
+    features:[
+      {name:'5  Users',enabled:true},
+      {name:'50GB Storage',enabled:true},
+      {name:'Unlimited Public Projects',enabled:true},
+      {name:'Community Access',enabled:true},
+      {name:'Unlimited Private Projects',enabled:true},
+      {name:'Dedicated Phone Support',enabled:true},
+      {name:'Free Subdomain',enabled:true},
+      {name:'Monthly Status Reports',enabled:false}
+    ]
+  },
+  {
+    name:"PRO",
+    price:49,
+    features:[
+      {name:'Unlimited  User',enabled:true},
+      {name:'150GB Storage',enabled:true},
+      {name:'Unlimited Public Projects',enabled:true},
+      {name:'Community Access',enabled:true},
+      {name:'Unlimited Private Projects',enabled:true},
+      {name:'Dedicated Phone Support',enabled:true},
+      {name:'Free Subdomain',enabled:true},
+      {name:'Monthly Status Reports',enabled:true}
+    ]
+  }
+  ]
+  return (
+    <section class="pricing py-5">
+    <div class="container">
+      <div class="row">
+   {
+     plans.map((plan,index)=>{
+       return <Plans key={index} name={plan.name} price={plan.price} features={plan.features}/>
+     })
+   }
+        </div>
+        </div>
+        </section>
+  );
+}
+
+export default App;
